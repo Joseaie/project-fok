780f3e4d0a446d7ed775faa5534c587a83670d8c
Korinna Pinakoulakis <kpinakoulaki@gmail.com>
2022-09-29 15:37:28 +0100

    add GET all action with TDD

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function () {
-  console.log('listening on 3000');
+const books = [
+  { id: 1, title: 'Book1', author: 'M. Garfield', rating: 5 },
+  { id: 2, title: 'Book2', author: 'E. Penny', rating: 3 }
+];
+
+// GET /books
+app.get('/books', (req, res) => {
+  res.send(books);
+})
+
+app.listen(8080, function () {
+  console.log('listening on 8080');
 });
+
+module.exports = app;
--- server.test.js
+++ server.test.js
@@ -1,7 +1,27 @@
-//const server = require('./server');
+const request = require('supertest');
+const server = require('./server');
 
 describe("test", () => {
   it("adds correctly", () => {
     expect(1 + 1).toBe(2)
+  })
+})
+
+const books = [
+  { id: 1, title: "Book1", author: "M. Garfield", rating: 5 },
+  { id: 2, title: "Book2", author: "E. Penny", rating: 3 }
+];
+
+describe("crud actions", () => {
+  it("/books displays all the books", () => {
+    return request(server)
+      .get('/books')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        expect(response.body).toEqual(
+          books
+        )
+      })
   })
 })
