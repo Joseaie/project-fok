ea825892cda4e0fa7dc1983a3657c635f0faaa10
Josephine Obeng <64155953+Joseaie@users.noreply.github.com>
2022-10-25 16:29:11 +0100

    Add PATCH test for one attribute

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
 const express = require('express');
 const app = express();
-const createError = require("http-errors");
+const createError = require('http-errors');
 
 app.use(express.json());
 
-const books = [
+let books = [
   { id: 1, title: 'Book1', author: 'M. Garfield', rating: 5 },
-  { id: 2, title: 'Book2', author: 'E. Penny', rating: 3 }
+  { id: 2, title: 'Book2', author: 'E. Penny', rating: 3 },
 ];
 
 // GET /books
 app.get('/books', (req, res) => {
   res.send(books);
-})
+});
 
 // GET /books/:id
 app.get('/books/:id', (req, res, next) => {
-  const book = books.find(book => book.id === parseInt(req.params.id));
+  const book = books.find((book) => book.id === parseInt(req.params.id));
   if (!book) {
-    return next(createError(404, "Not found!"));
+    return next(createError(404, 'Not found!'));
   }
   res.send(book);
-})
+});
 
 // POST /books
 app.post('/books', (req, res) => {
-
   const { title, author, rating } = req.body; // access multiple properties of an object and declare as variables all in one line
 
   const newBook = {
     title: title,
     author: author,
     rating: parseInt(rating),
-    id: books.length + 1
-  }
+    id: books.length + 1,
+  };
 
   books.push(newBook);
   res.status(201).json(newBook);
+});
 
-})
+// PATCH
+// { title: 'Flamingo' }
+app.patch('/books/:id', (req, res) => {
+  books;
+  // get books, compare ids, get correct book
+  // check property/ies being sent
+  // update the property of book record with what's been sent
+
+  const book = books.find((book) => book.id === parseInt(req.params.id));
+  const updatedTitle = req.body.title;
+  book.title = updatedTitle;
+
+  console.log(book);
+
+  res.send(book);
+  // question for Jasper (design)
+  // Approach 1: patch returns updated book
+  // one test only:
+  // update title -> check for 200 and check patch returns updated book
+
+  // OR
+
+  // Approach 2: patch does not return updated book
+  // first test:
+  // update title -> check that response is successful (200)
+  // second test (following first one):
+  // check that the get books/:id now returns the updated book
+});
 
 app.listen(8080, () => {
   console.log('listening on 8080');
--- app.test.js
+++ app.test.js
@@ -1,63 +1,85 @@
-const request = require("supertest");
-const app = require("./app");
+const request = require('supertest');
+const app = require('./app');
 
-describe("test", () => {
-  it("adds correctly", () => {
+describe('test', () => {
+  it('adds correctly', () => {
     expect(1 + 1).toBe(2);
   });
 });
 
 const books = [
-  { id: 1, title: "Book1", author: "M. Garfield", rating: 5 },
-  { id: 2, title: "Book2", author: "E. Penny", rating: 3 },
+  { id: 1, title: 'Book1', author: 'M. Garfield', rating: 5 },
+  { id: 2, title: 'Book2', author: 'E. Penny', rating: 3 },
 ];
 
-describe("crud actions", () => {
-  it("GET /books displays all the books", () => {
+describe('crud actions', () => {
+  it('GET /books displays all the books', () => {
     return request(app)
-      .get("/books")
+      .get('/books')
       .expect(200)
-      .expect("Content-Type", /json/)
+      .expect('Content-Type', /json/)
       .then((response) => {
         expect(response.body).toEqual(books);
       });
   });
-  
+
   // GET /book/:id - displays a requested book
-  it("GET /books/1 displays book 1", () => {
+  it('GET /books/1 displays book 1', () => {
     return request(app)
-      .get("/books/1")
+      .get('/books/1')
       .expect(200)
-      .expect("Content-Type", /json/)
+      .expect('Content-Type', /json/)
       .then((response) => {
         expect(response.body).toEqual(books.find((book) => book.id === 1));
       });
   });
 
   // GET /books/:id - returns 404 when book not found
-  it("GET /books/999 returns error", () => {
-    return request(app).get("/books/999").expect(404)
+  it('GET /books/999 returns error', () => {
+    return request(app).get('/books/999').expect(404);
   });
 
   // POST /books - the book is created
-  it("POST /books creates a book", () => {
+  it('POST /books creates a book', () => {
     return request(app)
-      .post("/books")
+      .post('/books')
       .expect(201)
-      .send({title: "Book title", author: "JB Fletcher", rating: 4})
-      .expect("Content-Type", /json/)
+      .send({ title: 'Book title', author: 'JB Fletcher', rating: 4 })
+      .expect('Content-Type', /json/)
       .then((response) => {
         expect(response.body).toEqual(
           expect.objectContaining({
             title: expect.any(String),
             author: expect.any(String),
-            rating: expect.any(Number)
+            rating: expect.any(Number),
           })
         );
       });
   });
 
-  // POST /books error if title not string 
+  // (implement maybe later) POST /books error if title not string
 
+  // PATCH /books/:id tests
+  // given only one property (e.g. title) => return 200 & changes property correctly & doesn't change/overwrite the other fields
+  it('PATCH /books/:id given only one property it only updates given property', () => {
+    return request(app)
+      .patch('/books/1')
+      .expect(200)
+      .send({ title: 'Flamingo' })
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: 1,
+            title: 'Flamingo',
+            author: 'M. Garfield',
+            rating: 5,
+          })
+        );
+      });
+  });
+  // given all possible properties => return 200 & changes all of them correctly
+  // given invalid data => nothing should be updated & return 400 (Bad Request)
+  // book doesn't exist => nothing should be updated & return 404 (Not found)
 });
 
