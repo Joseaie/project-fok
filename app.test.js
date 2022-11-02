69376e211917df29cf926a55a1888f53223494a3
Josephine Obeng <64155953+Joseaie@users.noreply.github.com>
2022-11-02 12:56:44 +0000

    Revert changes from app.test.js

--- app.test.js
+++ app.test.js
@@ -1,61 +1,63 @@
-const request = require('supertest');
-const app = require('./app');
+const request = require("supertest");
+const app = require("./app");
 
-describe('test', () => {
-  it('adds correctly', () => {
+describe("test", () => {
+  it("adds correctly", () => {
     expect(1 + 1).toBe(2);
   });
 });
 
 const books = [
-  { id: 1, title: 'Book1', author: 'M. Garfield', rating: 5 },
-  { id: 2, title: 'Book2', author: 'E. Penny', rating: 3 },
+  { id: 1, title: "Book1", author: "M. Garfield", rating: 5 },
+  { id: 2, title: "Book2", author: "E. Penny", rating: 3 },
 ];
 
-describe('crud actions', () => {
-  it('GET /books displays all the books', () => {
+describe("crud actions", () => {
+  it("GET /books displays all the books", () => {
     return request(app)
-      .get('/books')
+      .get("/books")
       .expect(200)
-      .expect('Content-Type', /json/)
+      .expect("Content-Type", /json/)
       .then((response) => {
         expect(response.body).toEqual(books);
       });
   });
-
+  
   // GET /book/:id - displays a requested book
-  it('GET /books/1 displays book 1', () => {
+  it("GET /books/1 displays book 1", () => {
     return request(app)
-      .get('/books/1')
+      .get("/books/1")
       .expect(200)
-      .expect('Content-Type', /json/)
+      .expect("Content-Type", /json/)
       .then((response) => {
         expect(response.body).toEqual(books.find((book) => book.id === 1));
       });
   });
 
   // GET /books/:id - returns 404 when book not found
-  it('GET /books/999 returns error', () => {
-    return request(app).get('/books/999').expect(404);
+  it("GET /books/999 returns error", () => {
+    return request(app).get("/books/999").expect(404)
   });
 
   // POST /books - the book is created
-  it('POST /books creates a book', () => {
+  it("POST /books creates a book", () => {
     return request(app)
-      .post('/books')
+      .post("/books")
       .expect(201)
-      .send({ title: 'Book title', author: 'JB Fletcher', rating: 4 })
-      .expect('Content-Type', /json/)
+      .send({title: "Book title", author: "JB Fletcher", rating: 4})
+      .expect("Content-Type", /json/)
       .then((response) => {
         expect(response.body).toEqual(
           expect.objectContaining({
             title: expect.any(String),
             author: expect.any(String),
-            rating: expect.any(Number),
+            rating: expect.any(Number)
           })
         );
       });
   });
 
-  // (implement maybe later) POST /books error if title not string
+  // POST /books error if title not string 
+
 });
+
