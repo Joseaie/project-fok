c4d6ec18b8ef99a48855266d0e39b956a445450a
Josephine Obeng <64155953+Joseaie@users.noreply.github.com>
2022-11-02 12:29:25 +0000

    Add books 'database' to another file

--- update.js
+++ update.js
@@ -1,9 +1,5 @@
 const app = require('./app');
-
-let books = [
-  { id: 1, title: 'Book1', author: 'M. Garfield', rating: 5 },
-  { id: 2, title: 'Book2', author: 'E. Penny', rating: 3 },
-];
+const books = require('./books');
 
 const update = app.patch('/books/:id', (request, response) => {
   const book = books.find((book) => book.id === parseInt(request.params.id));
